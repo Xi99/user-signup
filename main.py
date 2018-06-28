146502a2cae57f206d428dee5dc54cf56cb65073
Sean Lorman <sean.lorman@gmail.com>
2018-06-27 22:54:44 -0600

    have errors popping up now but in seperate window. Need to get in the same page

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
+@app.route("/", methods=['POST'])
+def validate():
+
+    check_username = request.form['username']
+    print(check_username)
+    if len(check_username) < 3:
+        return "Your username is either too short or too long"
+
 @app.route("/")
 def index():
     error= request.args.get("error")
