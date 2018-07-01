5c43a7ec824307d0e18bac7d21ab2ac03670a715
Sean Lorman <sean.lorman@gmail.com>
2018-07-01 14:32:24 -0600

    Working on getting all the validations working

--- main.py
+++ main.py
@@ -10,11 +10,28 @@ app.config['DEBUG'] = True
 def validate():
 
     check_username = request.form['username']
-    print(check_username)
-    if len(check_username) < 3:
-        return "Your username is either too short or too long"
+    check_password = request.form['password']
+    check_verify_password = request.form["verify password"]
+    check_email = request.form["email"]
 
-@app.route("/")
+    
+    if "@" not in check_email:
+        error = "You must have a single @ in your emial"
+        return redirect("/?error=" + error)
+
+    if check_password is not check_verify_password:
+        error = "Your passwords do not match"
+        return redirect("/?error=" + error)
+    
+    if len(check_username) < 3 or len(check_username) > 20:
+        error = "Your username is either too short or too long"
+        return redirect("/?error=" + error)
+    
+    if check_username or check_password or check_verify_password is ' ':
+        error = "You must fill in all required fields"
+        return redirect("/?error=" + error)
+    
+@app.route("/") 
 def index():
     error= request.args.get("error")
 
