8b4a70c60ed505c1e33a48e80ba16e0153b06fba
Sean Lorman <sean.lorman@gmail.com>
2018-07-08 15:00:56 -0600

    Possibly broke it :)

--- main.py
+++ main.py
@@ -58,24 +58,28 @@ def validate():
     check_email = request.form["email"]
 
     if "@" not in check_email:
-        error = "You must have a single '@' and '.' in your emial"
-        return redirect("/?error=" + error)
+        email_error = "You must have a single '@' and '.' in your emial"
+        '''return redirect("/?error=" + email_error)'''
 
     if check_password != check_verify_password:
-        error = "Your passwords do not match"
-        return redirect("/?error=" + error)
+        password_again_error = "Your passwords do not match"
+        '''return redirect("/?error=" + password_again_error)'''
     
     if len(check_username) < 3 or len(check_username) > 20:
-        error = "Your username has to be between 3 and 20 characters long"
-        return redirect("/?error=" + error)
+        username_error = "Your username has to be between 3 and 20 characters long"
+        '''return redirect("/?error=" + username_error)'''
+    
+    if ' ' in check_username:
+        error = "You can't have a space in your username"
+        '''return redirect("/?error=" + error)'''
     
     if check_username or check_password or check_verify_password is ' ':
         error = "You must fill in all required fields"
-        return redirect("/?error=")
+        '''return redirect("/?error=" + error)'''
 
-    if not is_email(email):
-        error = email + '" is not a valid email address'
-        return redirect('/?error=' + error)
+    if not is_email(check_email):
+        email_error = 'is not a valid email address'
+        '''return redirect('/?error=' + email_error)'''
 
     else:
 
