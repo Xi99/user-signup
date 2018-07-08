b6d13248f7c1d9e1748c50e44f85556ffdf3a139
Sean Lorman <sean.lorman@gmail.com>
2018-07-08 13:10:46 -0600

    Have it working except the errors next to the specific rows. Also need to check all validations are working

--- main.py
+++ main.py
@@ -5,8 +5,51 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+def is_email(string):
 
-@app.route("/", methods=['POST'])
+    atsign_index = string.find('@')
+    atsign_present = atsign_index >= 0
+    if not atsign_present:
+        return False
+    else:
+        domain_dot_index = string.find('.', atsign_index)
+        domain_dot_present = domain_dot_index >= 0
+        return domain_dot_present
+
+'''@app.route("/welcome", methods=['GET', 'POST'])
+def welcome():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify password']
+        
+    username = request.form['username']
+        
+    
+
+    if len(check_username) < 3 or len(check_username) > 20:
+        error = "Your username has to be between 3 and 20 characters long"
+        return redirect("/" + error)
+
+    if not is_email(email):
+        error = email + '" is not a valid email address'
+        return redirect('/' + error)
+        
+    if password != verify:
+        error = 'Your passwords do not match'
+        return redirect('/' + error)
+        
+    if check_username or check_password or check_verify_password is ' ':
+        error = "You must fill in all required fields"
+        return redirect("/" + error)
+
+    else:
+    return render_template(
+        'welcome.html',
+        username=username
+        )
+'''
+@app.route("/welcome", methods=['GET', 'POST'])
 def validate():
 
     check_username = request.form['username']
@@ -14,23 +57,33 @@ def validate():
     check_verify_password = request.form["verify password"]
     check_email = request.form["email"]
 
-    
     if "@" not in check_email:
-        error = "You must have a single @ in your emial"
+        error = "You must have a single '@' and '.' in your emial"
         return redirect("/?error=" + error)
 
-    if check_password is not check_verify_password:
+    if check_password != check_verify_password:
         error = "Your passwords do not match"
         return redirect("/?error=" + error)
     
     if len(check_username) < 3 or len(check_username) > 20:
-        error = "Your username is either too short or too long"
+        error = "Your username has to be between 3 and 20 characters long"
         return redirect("/?error=" + error)
     
     if check_username or check_password or check_verify_password is ' ':
         error = "You must fill in all required fields"
-        return redirect("/?error=" + error)
-    
+        return redirect("/?error=")
+
+    if not is_email(email):
+        error = email + '" is not a valid email address'
+        return redirect('/?error=' + error)
+
+    else:
+
+        return render_template(
+            'welcome.html',
+            username=username
+            )
+
 @app.route("/") 
 def index():
     error= request.args.get("error")
