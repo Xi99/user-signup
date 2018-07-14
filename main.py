0fd530b15a54af6cb5db5c2990441f0ed306957b
Sean Lorman <sean.lorman@gmail.com>
2018-07-13 23:12:56 -0600

    got to work but working on why when password is blank it still allows to pass

--- main.py
+++ main.py
@@ -16,70 +16,72 @@ def is_email(string):
         domain_dot_present = domain_dot_index >= 0
         return domain_dot_present
 
-@app.route("/welcome", methods=['GET', 'POST'])
-def validate():
+@app.route('/')
+def index():
+    return render_template('edit.html')
 
-    username = request.form['username']
+
+@app.route("/welcome/<username>", methods=['GET', 'POST'])
+def welcome(username=None):
+    print('gotta UUsername woot')
+    print(username)
 
     return render_template(
         'welcome.html',
         username=username,
         )
 
-@app.route("/", methods=['GET', 'POST']) 
-def index():
 
-      
 
-    #.join or append here for them to show up next to field
-    
+@app.route("/submit", methods=['GET', 'POST']) 
+def submit():
+    print(request.form) 
     if request.method == 'GET':
-        error = request.args.get("error")
-        new_password_error = ''
-        new_username_error = ''
-        new_email_error = ''
-
+        #error = request.args.get("error")
         return render_template(
             'edit.html',
-            error=error
             )
+
     elif request.method == 'POST':
-        error = request.args.get("error")
         check_username = request.form['username']
         check_password = request.form['password']
         check_verify_password = request.form["verify password"]
         check_email = request.form["email"]
-       
+        
+        password_error = ''
+        email_error = ''
+        username_error = ''
+        none_error = ''
+
+
+        if check_password is ' ':
+            password_error = "Your passwords can't be blank"
+
         if check_password != check_verify_password:
             password_error = "Your passwords do not match"
-            return redirect("/?error=" + error)
+            
         if len(check_username) < 3 or len(check_username) > 20:
-            error = "Your username has to be between 3 and 20 characters long and have no space"
-            return redirect("/?error=" + error)
-        if ' ' in check_username:
-            error = "Your username has to be between 3 and 20 characters long and have no space"
-            return redirect("/?error=" + error)
-        if check_username or check_password or check_verify_password is ' ':
-            error = "You must fill in all required fields"
+            username_error = "Your username has to be between 3 and 20 characters long and have no space"
+            
+        if check_username and check_password and check_verify_password and check_email is ' ':
+            none_error = "You must fill in all required fields"
             
         if not is_email(check_email):
-            error = 'That is not a valid email address'
-            return redirect("/?error=" + error)
-  
-
-    else:
-            
-        return redirect('/welcome')
-        '''return render_template(
-        'welcome.html',
-        )'''
+            email_error = 'That is not a valid email address'
         
-            
-    #if no errors:
-    #    return redirect('/welcome')
+       
+        if ' ' in check_username:
+            username_error = "Your username has to be between 3 and 20 characters long and have no space"
+
+        if not username_error and not password_error and not email_error:
+            return redirect('/welcome/' + request.form['username'])
+        else:
+            return render_template('edit.html', username=check_username, email=check_email, username_error=username_error, email_error=email_error, password_error=password_error, none_error=none_error)
 
     
-    
+        
+        
+                    
 
 
 app.run()
